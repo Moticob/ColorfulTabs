9cefd36ef764cf2bf6a68fdfe27b6837e737ce30
Moticob <97236123+Moticob@users.noreply.github.com>
2024-07-06 20:09:20 +0100

    Create content.js

---
+++ content.js
@@ -0,0 +1,3 @@
+const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF33A8'];
+const color = colors[Math.floor(Math.random() * colors.length)];
+document.body.style.backgroundColor = color;
