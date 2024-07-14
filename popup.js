1757aa0be1cc07b60cc11113e4592c065f6fedf6
Moticob <oussama.ed-derouach@outlook.com>
2024-07-13 17:43:42 -0700

    add

--- popup.js
+++ popup.js
@@ -7,6 +7,10 @@ document.getElementById('changeColor').addEventListener('click', () => {
   });
 });
 
+
+
+
+
 function changeColor() {
   const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF33A8'];
   const color = colors[Math.floor(Math.random() * colors.length)];
